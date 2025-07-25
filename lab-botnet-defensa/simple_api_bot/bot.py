432d7c06d16c0b9aefa89d1c2fb69d88981234d3
sdailoff <sofia.dailoff1@gmail.com>
2025-07-25 16:15:26 -0300

    bot.py envv

--- lab-botnet-defensa/simple_api_bot/bot.py
+++ lab-botnet-defensa/simple_api_bot/bot.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-API_KEY = os.getenv("API_KEY")
-API_KEY_SECRET = os.getenv("API_KEY_SECRET")
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+API_KEY = os.getenv("KccXm6xPvNdOeZCcYorcKfCNI")
+API_KEY_SECRET = os.getenv("Zj3qOHhM1uFRWorCbF10lZ62HRZqQ364qHkVY0FJEjkMEyIebJ")
+ACCESS_TOKEN = os.getenv("1776367416546656257-Ubfi6U46fdh7lZzR0QQzZom1Wqbz10")
+ACCESS_TOKEN_SECRET = os.getenv("UkW4vUCkXkRYMoJhGPIvuBenuYG8S0uTKtutxmBKkUtXy")
